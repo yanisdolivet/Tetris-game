b3d1db62c9aba5f68e6f48e73e1864002130a8d7
yanisdolivet <yanis.dolivet@epitech.eu>
2026-01-13 14:27:34 +0100

    feat: init window, keybind, system and component of the engine

--- include/Game.hpp
+++ include/Game.hpp
@@ -7,27 +7,85 @@
 
 #pragma once
 
+#include <AnimationComponent.hpp>
+#include <AnimationSystem.hpp>
+#include <Collider.hpp>
+#include <CollisionSystem.hpp>
+#include <Core.hpp>
+#include <DrawableComponent.hpp>
+#include <EngineSubscriptions.hpp>
+#include <GameEngineEvents.hpp>
+#include <InitActionInput.hpp>
+#include <InputReleasedSystem.hpp>
+#include <InputSystem.hpp>
+#include <Movement.hpp>
+#include <MovementSystem.hpp>
+#include <Parallax.hpp>
+#include <PhysicsSystem.hpp>
+#include <Position.hpp>
+#include <Raylib.hpp>
+#include <RenderSystem.hpp>
+#include <Speed.hpp>
+#include <SpriteComponent.hpp>
+#include <Velocity.hpp>
+#include <memory>
+
 #include "Logs.hpp"
 
-namespace Tetris {
-    class Game {
+namespace Tetris
+{
+    class Game
+    {
         public:
             Game();
             ~Game();
 
             /*
-            * @brief Initialize the game
-            * @return void
-            */
+             * @brief Initialize the game
+             * @return void
+             */
             void init();
 
             /*
-            * @brief Run the game loop
-            * @return void
-            */
+             * @brief Run the game loop
+             * @return void
+             */
             void run();
 
         protected:
         private:
+            /*
+             * @brief Initialize game entities
+             * @return void
+             */
+            void _initEntities();
+
+            /*
+             * @brief Initialize game components
+             * @return void
+             */
+            void _initComponents();
+
+            /*
+             * @brief Initialize game systems
+             * @return void
+             */
+            void _initSystems();
+
+            /*
+             * @brief Initialize keybinds
+             * @return void
+             */
+            void _initKeybinds();
+
+            /*
+             * @brief Initialize event subscriptions
+             * @return void
+             */
+            void _initSubscriptions();
+
+        private:
+            std::shared_ptr<Graphic::Raylib> _graphic;
+            GameEngine::Core _engine;
     };
-}
+} // namespace Tetris
--- src/Game.cpp
+++ src/Game.cpp
@@ -11,6 +11,7 @@ namespace Tetris
 {
     Game::Game()
     {
+        this->_graphic = std::make_shared<Graphic::Raylib>();
     }
 
     Game::~Game()
@@ -19,12 +20,104 @@ namespace Tetris
 
     void Game::init()
     {
+        this->_graphic->initWindow(800, 600, "Tetris Game");
+        this->_initComponents();
+        this->_initSystems();
+        this->_initEntities();
+        this->_initKeybinds();
+        this->_initSubscriptions();
+
         LOG_INFO("Game initialized");
     }
 
     void Game::run()
     {
+        this->_engine.start();
+        this->_engine.run();
         LOG_INFO("Game Runnning");
+    }
+
+    void Game::_initEntities()
+    {
+        LOG_INFO("Initializing game entities");
+    }
+
+    void Game::_initComponents()
+    {
+        // Register all the components used in the game ex:
+        this->_engine.getRegistry().registerComponent<Components::Position>();
+        this->_engine.getRegistry().registerComponent<Components::Velocity>();
+        this->_engine.getRegistry().registerComponent<Components::Collider>();
+        this->_engine.getRegistry().registerComponent<Components::Speed>();
+        this->_engine.getRegistry().registerComponent<Components::Movement>();
+        this->_engine.getRegistry().registerComponent<Components::DrawableComponent>();
+        this->_engine.getRegistry().registerComponent<Components::SpriteComponent>();
+        this->_engine.getRegistry().registerComponent<Components::AnimationComponent>();
+        this->_engine.getRegistry().registerComponent<Components::Parallax>();
+        LOG_INFO("Initializing game components");
+    }
+
+    void Game::_initSystems()
+    {
+        // Animation System
+        this->_engine.getRegistry()
+            .addSystem<Components::DrawableComponent, Components::Position, Components::AnimationComponent,
+                       Components::Parallax>(AnimationSystem());
+
+        // Rendering System
+        this->_engine.getRegistry()
+            .addSystem<Components::Position, Components::SpriteComponent, Components::DrawableComponent>(
+                RenderSystem(this->_graphic));
+
+        // Input System
+        this->_engine.getRegistry().addSystem<>(
+            InputSystem(this->_graphic->getKeymap(), this->_graphic->getKeyDownFunc()));
+
+        // Input Released System
+        this->_engine.getRegistry().addSystem<>(
+            InputReleasedSystem(this->_graphic->getKeyReleasedMap(), this->_graphic->getKeyReleasedFunc()));
+
+        // Movement System
+        this->_engine.getRegistry().addSystem<Components::Movement, Components::Velocity, Components::Speed>(
+            MovementSystem());
+
+        // Physics System
+        this->_engine.getRegistry().addSystem<Components::Position, Components::Velocity>(PhysicsSystem());
+
+        // Collision System
+        this->_engine.getRegistry().addSystem<Components::Position, Components::Collider>(CollisionSystem());
+
+        LOG_INFO("Initializing game systems");
+    }
+
+    void Game::_initSubscriptions()
+    {
+        Common::initEngineSubscriptions(this->_engine);
+        LOG_INFO("Initializing event subscriptions");
+    }
+
+    void Game::_initKeybinds()
+    {
+
+        const std::unordered_map<std::string, ActionBinding> actionBindings = getActionBindings();
+
+        int key = this->_graphic->stringtoKeyCode("KEY_ESCAPE");
+
+        auto it = actionBindings.find("ExitGame");
+        if (it == actionBindings.end())
+            throw std::runtime_error("Unknown action: ExitGame");
+        const ActionBinding& action = it->second;
+
+        if (action.onPress) {
+            LOG_INFO("Adding action to key {}", key);
+            this->_graphic->addKeyMapping(key, action.onPress);
+        }
+
+        if (action.onRelease) {
+            this->_graphic->addKeyReleasedMapping(key, action.onRelease);
+        }
+
+        LOG_INFO("Initializing keybinds");
     }
 
 } // namespace Tetris
