048e01d0538ef0fbfd475e70242a6f29a8748eaf
yanisdolivet <yanis.dolivet@epitech.eu>
2026-01-13 11:21:49 +0100

    initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2026
+** Tetris-game
+** File description:
+** main
+*/
+
+#include <iostream>
+
+int main(void)
+{
+    std::cout << "Hello, Tetris!" << std::endl;
+    return 0;
+}
